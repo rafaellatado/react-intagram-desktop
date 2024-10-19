11e7285a546f0c23ab862a18e23dccfebb4f75a0
rafaellatado <latadorafael@gmail.com>
2024-10-19 18:29:37 -0300

    useState img

--- src/components/User.js
+++ src/components/User.js
@@ -3,9 +3,10 @@ import { useState } from "react";
 export default function User() {
 
   const [userName, setUserName] = useState('catanacomics');
+  const [profileImg, setProfileImg] = useState('assets/img/catanacomics.svg');
 
   const handleUserName = () => {
-    let username = prompt('Type your username:');
+    let username = prompt('Enter your username:');
     
     if (username === '') {
       return;
@@ -14,9 +15,19 @@ export default function User() {
     }
   }
 
+  const handleProfileImg = () => {
+    let image = prompt('Enter an image URL:')
+
+    if (image === '') {
+      return;
+    } else {
+      setProfileImg(image);
+    }    
+  }
+
   return (
     <div class="usuario">
-      <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+      <img src={profileImg} alt="imagem de perfil" onClick={handleProfileImg}/>
       <div class="texto">
         <span>
           <strong>{userName}</strong>

162cc9fd3cc6c097d7113716785a754b4503735b
rafaellatado <latadorafael@gmail.com>
2024-10-19 14:26:19 -0300

    first-commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Content from "./Content";
+import NavBar from "./NavBar";
+
+export default function App() {
+    return (
+        <div>
+            <NavBar />
+            <Content />
+        </div>
+    )
+}

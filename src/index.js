d1d27bb3f6aa1ccd6f21b77674e31eb8714f8574
rafaellatado <latadorafael@gmail.com>
2025-03-17 17:48:59 -0300

    README.me

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-/* import ReactDOM from "react-dom";
-import App from "./components/App"
-
-ReactDOM.render(<App />, document.querySelector(".root")); */
-
 import ReactDOM from "react-dom/client"; 
 import App from "./components/App";
 
